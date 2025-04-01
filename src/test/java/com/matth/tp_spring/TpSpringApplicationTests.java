f4b134a953bfe0146919c68a0da54295f1bd1ef9
matthiasm8 <matthias.muyl@gmail.com>
2025-04-01 14:28:16 +0200

    creation du tp_spring

---
+++ src/test/java/com/matth/tp_spring/TpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matth.tp_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
